b6de860b99e72778b70d131ad7b7f057f46b6816
Victor MICHAUX <20160557@etudiants.campus.villejuif>
2016-11-07 11:06:02 +0100

    ajout des fonctions pour la main

--- src/BlackJackConsole.java
+++ src/BlackJackConsole.java
@@ -3,33 +3,22 @@ public class BlackJackConsole {
 
 	public BlackJackConsole() {
 		System.out.println("Welcome to the BlackJack table. Let's play !");
-		Card[] tab = { new Card(Value.TWO, Color.HEART), new Card(Value.JACK, Color.SPADE) };
-		for(Card c : tab) { // For each card
-			System.out.println("This card is a "+c+ " worth "+c.getPoints()+ " points");
-			System.out.print("It's a ");
-			switch(c.getColorSymbole()) { // Ok from Java 1.7
-				case "♥": System.out.print("heart"); break;
-				case "♠": System.out.print("spade"); break;
-				case "♣": System.out.print("club"); break;
-				case "♦": System.out.print("diamond"); break;
-			}
-			if(c.getValueSymbole().matches("[JQK]")) { // Is the value symbole a J or a Q or a K ?
-				System.out.println(" and a face !");
-			} else {
-				System.out.println(" and it's not a face.");
-			}
-		}
 		Deck deck = new Deck(2);
-		System.out.println( "Here is the deck "+ deck);
-		for(int i = 0 ; i < 3 ; i++){
-				try{
-					Card c = deck.draw();
-					System.out.println("This card is a " + c + " worth "+c.getPoints()+ " points");
-				} catch (EmptyDeckException ex){
-					System.err.println(ex.getMessage());
-					System.exit(-1);
-				}
+		Hand hand = new Hand();
+		System.out.println("Your hand is currently : "+ hand);
+		for(int i=0; i < 3 ; i++) {
+			try {
+				hand.add(deck.draw());
+			} catch (EmptyDeckException ex) {
+				System.err.println(ex.getMessage());
+				System.exit(-1);
+			}
 		}
+		System.out.println("Your hand is currently : "+ hand);
+		System.out.println("The best score is : " + hand.best());
+		hand.clear();
+		System.out.println("Your hand is currently : "+ hand);
+
 	}
 	
 	
--- src/Hand.java
+++ src/Hand.java
@@ -1,5 +1,48 @@
 import java.util.LinkedList;
+import java.util.List;
 
 public class Hand {
-private LinkedList<Card> cardList;
+	private LinkedList<Card> cardList;
+	
+	public Hand() {
+		this.cardList = new LinkedList<Card>();
+	}
+	
+	@Override
+	public String toString() {
+		return this.cardList.toString() + " : " + count().toString();
+	}
+	
+	public void add(Card card) {
+		this.cardList.add(card);
+	}
+	
+	public void clear() {
+		this.cardList.removeAll(this.cardList);
+	}
+	
+	public List<Integer> count() {
+		LinkedList<Integer> resultat = new LinkedList<Integer>();
+		resultat.add(0);
+		for(Card card : this.cardList) {
+			int size = resultat.size();
+			for(int i = 0; i < size; i++) {
+				int val = resultat.get(i);
+				resultat.set(i,	val + card.getPoints());
+				if(card.getPoints() == 1) resultat.add(val + 11);
+			}
+		}
+		return resultat;
+	}
+	
+	public int best() {
+		int max = 0;
+		int defaut = 0;
+		for(Integer i : count()) {
+			i = i.intValue();
+			if(i > max) defaut = i;
+			if(i <= 21) max = i;
+		}
+		return (max == (int) 0) ? defaut : max;
+	}
 }
